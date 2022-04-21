279319af0e5ca94d149708b393db73724593ab90
jms <bde574786@naver.com>
2022-04-21 17:30:50 +0900

    소켓통신 단방향, 양방향

---
+++ chapter04/src/socket_ex/ch02/ClientFile.java
@@ -0,0 +1,57 @@
+package socket_ex.ch02;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+/**
+ * 입력 여러번 받기<br>
+ * while문 사용
+ * @author ITPS
+ *
+ */
+public class ClientFile {
+
+	Socket socket;
+	BufferedWriter bufferedWriter;
+
+	final String IP = "192.168.4.10";
+	final int PORT = 10000;
+
+	BufferedReader keybordBufferedReader;
+
+	public ClientFile() {
+
+		try {
+			System.out.println("1. 클라이언트 소켓 시작");
+			socket = new Socket(IP, PORT);
+
+			System.out.println("2. 버퍼 연결");
+			bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+
+			System.out.println("3. 키보드 버퍼 연결");
+			keybordBufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+			while (true) {
+				System.out.println("4. 키보드 입력 대기");
+				String msg = keybordBufferedReader.readLine();
+
+				bufferedWriter.write(msg + "\n");
+				bufferedWriter.flush();
+			}
+
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		new ClientFile();
+	}
+}
--- chapter04/src/socket_ex/ch02/ServerFile.java
+++ chapter04/src/socket_ex/ch02/ServerFile.java
@@ -1,4 +1,4 @@
-package socket_ex.ch01;
+package socket_ex.ch02;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -8,17 +8,6 @@ import java.net.Socket;
 
 public class ServerFile {
 
-	/**
-	 * 두개가 필요한 이유<br>
-	 * 1. 실행되는 서버 소켓 ㅡ> 연결만 받는다. ( 연결만 해주고 연결되면 연결을 끊고 다른 연결 기다림 )<br>
-	 * 2. 실행되는 클라이언트 소켓<br>
-	 * 3. 둘 사이 연결 시도 ( 바이트 스트림으로 연결 )<br>
-	 * 4. 새로운 소켓을 생성 <br>
-	 * 1024 ~ 65535 까지중 사용하지 않는 포트 번호를 랜덤으로 생성해준다.<br>
-	 * 
-	 * 
-	 */
-	// 소켓 사용시 두개가 필요
 	ServerSocket serverSocket;
 	Socket socket;
 
@@ -31,14 +20,16 @@ public class ServerFile {
 			serverSocket = new ServerSocket(10000);
 			System.out.println("2. 서버 소켓 생성 완료");
 
-			// 클라이언트 연결 대기하는 녀석
 			socket = serverSocket.accept();
 			System.out.println("3. 클라이언트 연결 완료");
 
 			bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
-			String msg = bufferedReader.readLine();
-			System.out.println("4. 클라이언트로 받은 메세지 : " + msg);
+			while (true) {
+				String msg = bufferedReader.readLine();
+				System.out.println("4. 클라이언트로 받은 메세지 : " + msg);
+			}
+
 		} catch (IOException e) {
 			e.printStackTrace();
 			System.out.println("예외발생");
